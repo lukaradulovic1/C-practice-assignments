7f2f5baa2f7831d66993442b9cf308e7b2183dcd
lukaradulovic1 <luka.radulovic.011@gmail.com>
2021-08-30 13:48:31 +0200

    hotel tweaks

---
+++ hotel_composition_oop_singleton/hotel_composition_oop_singleton/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hotel_composition_oop_singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
