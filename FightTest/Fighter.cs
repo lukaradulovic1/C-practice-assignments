9667d3e0e56ebeffcacf5d74d411a5fd2879aef4
lukaradulovic1 <85028352+lukaradulovic1@users.noreply.github.com>
2021-08-31 16:35:10 +0200

    Add files via upload

---
+++ FightTest/Fighter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    class Fighter
+    {
+        private int healthPoints;
+        private int damagePoints;
+        private string name;
+
+        public int HealthPoints { get => healthPoints; set => healthPoints = value; }
+        public int DamagePoints { get => damagePoints; set => damagePoints = value; }
+        public string Name { get => name; set => name = value; }
+
+        public Fighter(int healthPoints, int damagePoints, string name)
+        {
+            this.HealthPoints = healthPoints;
+            this.DamagePoints = damagePoints;
+            this.Name = name;
+        }
+
+        public void IsAttacking(Fighter fighter)
+        {
+        fighter.HealthPoints = fighter.HealthPoints - fighter.DamagePoints;
+            Console.WriteLine($"{fighter.Name} now has {HealthPoints}");
+
+        }
+
+
+
+    }
+
---
+++ FightTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+
+class Program
+{
+
+    static void Main()
+    {
+        Arena arena = new Arena();
+        Fighter firstDude = new Fighter(100, 10, "Fedor");
+        Fighter secondDude = new Fighter(100, 30, "Cain Velasques");
+
+        bool someoneLost = false;
+
+
+        arena.PrintFighterStats(firstDude);
+        arena.PrintFighterStats(secondDude);
+        while (firstDude.HealthPoints > 0)
+        {
+            arena.Fight(firstDude, secondDude);
+
+        }
+        
+    }
+}
+
