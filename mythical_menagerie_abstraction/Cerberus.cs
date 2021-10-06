3e0c6f2d24d654ac059e06baae5b3d67b314a7a7
lukaradulovic1 <luka.radulovic.011@gmail.com>
2021-10-06 15:35:32 +0200

    a

--- mythical_menagerie_abstraction/Cerberus.cs
+++ mythical_menagerie_abstraction/Cerberus.cs
@@ -8,7 +8,44 @@ namespace mythical_menagerie_abstraction
 {
     public class Cerberus : MythicalCreature
     {
+        public override string Name
+        {
+            get
+            {
+                return Name;
+            }
+        }
 
-        
+        public override string AffiliatedDiety
+        {
+            get
+            {
+                return AffiliatedDiety;
+            }
+        }
+
+        public override string Area
+        {
+            get
+            {
+                return Area;
+            }
+        }
+
+        public override string Alignment
+        {
+            get
+            {
+                return Alignment;
+            }
+        }
+
+        public override string SpecialAbility
+        {
+            get
+            {
+                return SpecialAbility;
+            }
+        }
     }
 }
