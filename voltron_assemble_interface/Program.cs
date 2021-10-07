a42c126b2196199908e08507cf846ec914bddbbf
lukaradulovic1 <luka.radulovic.011@gmail.com>
2021-10-07 23:30:09 +0200

    v

---
+++ voltron_assemble_interface/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace voltron_assemble_interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
