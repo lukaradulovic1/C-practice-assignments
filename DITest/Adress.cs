60f81621e730a1ae67b935ced63f1f619d410c6a
lukaradulovic1 <luka.radulovic.011@gmail.com>
2021-11-15 17:08:03 +0100

    Corrected constructor code, added constructors for Adress and Person, override of ToString methods

---
+++ DITest/Adress.cs
@@ -0,0 +1,30 @@
+﻿namespace DITest
+{
+    public class Adress
+    {
+        private string adressInfo;
+
+        public Adress(string adressInfo)
+        {
+            AdressInfo = adressInfo;
+            
+        }
+
+        public string AdressInfo
+        {
+            get
+            {
+                return this.adressInfo;
+            }
+            set 
+            { 
+                this.adressInfo = value; 
+            }
+
+        }
+        public override string ToString()
+        {
+            return this.adressInfo;
+        }
+    }
+}
---
+++ DITest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DITest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type in the persons name, age and adress");
+            var name = Console.ReadLine();
+            var age = int.Parse(Console.ReadLine());
+            var adress = new Adress(Console.ReadLine());
+           
+
+            var person = new Person (name, age, adress);
+            Console.WriteLine(person.ToString());
+        }
+    }
+
+
+
+}
