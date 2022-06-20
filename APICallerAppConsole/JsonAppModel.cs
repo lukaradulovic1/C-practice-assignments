781f5ba17960397c47bf09c589f8170a2a155179
Luka_Radulovic <luka.radulovic.011@gmail.com>
2022-06-20 09:33:39 +0200

    added timeout, added get, post methods, log link method and joined output

---
+++ APICallerAppConsole/JsonAppModel.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace APICallerAppConsole
+{// Root myDeserializedClass = JsonSerializer.Deserialize<List<Root>>(myJsonResponse);
+    public class Branch
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("commit")]
+        public Commit Commit { get; set; }
+
+        [JsonPropertyName("protected")]
+        public bool Protected { get; set; }
+
+        [JsonPropertyName("protection")]
+        public Protection Protection { get; set; }
+
+        [JsonPropertyName("protection_url")]
+        public string ProtectionUrl { get; set; }
+    }
+
+    public class Commit
+    {
+        [JsonPropertyName("sha")]
+        public string Sha { get; set; }
+
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+    }
+
+    public class LastBuild
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("buildNumber")]
+        public string BuildNumber { get; set; }
+
+        [JsonPropertyName("queueTime")]
+        public DateTime QueueTime { get; set; }
+
+        [JsonPropertyName("startTime")]
+        public DateTime StartTime { get; set; }
+
+        [JsonPropertyName("finishTime")]
+        public DateTime FinishTime { get; set; }
+
+        [JsonPropertyName("lastChangedDate")]
+        public DateTime LastChangedDate { get; set; }
+
+        [JsonPropertyName("status")]
+        public string Status { get; set; }
+
+        [JsonPropertyName("result")]
+        public string Result { get; set; }
+
+        [JsonPropertyName("reason")]
+        public string Reason { get; set; }
+
+        [JsonPropertyName("sourceBranch")]
+        public string SourceBranch { get; set; }
+
+        [JsonPropertyName("sourceVersion")]
+        public string SourceVersion { get; set; }
+
+        [JsonPropertyName("tags")]
+        public List<string> Tags { get; set; }
+
+        [JsonPropertyName("properties")]
+        public Properties Properties { get; set; }
+    }
+
+    public class Properties
+    {
+    }
+
+    public class Protection
+    {
+        [JsonPropertyName("enabled")]
+        public bool Enabled { get; set; }
+
+        [JsonPropertyName("required_status_checks")]
+        public RequiredStatusChecks RequiredStatusChecks { get; set; }
+    }
+
+    public class RequiredStatusChecks
+    {
+        [JsonPropertyName("enforcement_level")]
+        public string EnforcementLevel { get; set; }
+
+        [JsonPropertyName("contexts")]
+        public List<object> Contexts { get; set; }
+
+        [JsonPropertyName("checks")]
+        public List<object> Checks { get; set; }
+    }
+
+    public class Root
+    {
+        [JsonPropertyName("branch")]
+        public Branch Branch { get; set; }
+
+        [JsonPropertyName("configured")]
+        public bool Configured { get; set; }
+
+        [JsonPropertyName("lastBuild")]
+        public LastBuild LastBuild { get; set; }
+
+        [JsonPropertyName("trigger")]
+        public string Trigger { get; set; }
+    }
+}
+
+
---
+++ APICallerAppConsole/Program.cs
@@ -0,0 +1,123 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+
+namespace APICallerAppConsole
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Deserailize first JSON list of branches, get json branch model
+            var stringToDeserialize = new Program().GetBranchesList();
+            var jsonBranchModelList = JsonConvert.DeserializeObject<List<Root>>(stringToDeserialize);
+            var branchNamesList = jsonBranchModelList.Select(x => x.Branch.Name).ToList();
+            var branchNameLastBuild = string.Empty;
+            var buildStatus = string.Empty;
+
+            var buildLogFromBetaBranch = string.Empty;
+
+            // Deserailize and get latest build info after posting
+            var stringToDeserializeUpdated = string.Empty;
+            List<Root> jsonBranchModelListUpdated = new List<Root>();
+
+            foreach (var branch in branchNamesList)
+            {
+                branchNameLastBuild = jsonBranchModelList.Where(x => x.Branch.Name.Contains(branch)).Select(x => x.LastBuild.BuildNumber).FirstOrDefault();
+                buildStatus = jsonBranchModelList.Where(x => x.Branch.Name.Contains(branch)).Select(x => x.LastBuild.Status).FirstOrDefault();
+
+                // Create build
+                Console.WriteLine($"Calling Post method that creates a new build on {branch} branch");
+                PostCreateBuild(branch);
+
+                Thread.Sleep(300000);
+
+                // Update build info from branch
+
+                stringToDeserializeUpdated = new Program().GetBranchesList();
+
+                jsonBranchModelListUpdated = JsonConvert.DeserializeObject<List<Root>>(stringToDeserializeUpdated);
+
+                branchNameLastBuild = jsonBranchModelListUpdated.Where(x => x.Branch.Name.Contains(branch)).Select(x => x.LastBuild.BuildNumber).FirstOrDefault();
+
+                buildStatus = jsonBranchModelListUpdated.Where(x => x.Branch.Name.Contains(branch)).Select(x => x.LastBuild.Status).FirstOrDefault();
+                if (buildStatus.Contains("Completed"))
+                {
+                    Console.WriteLine($"Build ID: {branchNameLastBuild} for Branch: {branch} status: {buildStatus} \nBuild log link: {GetBuildLogsLink(branchNameLastBuild)}\n");
+                }
+                else
+                {
+                    Thread.Sleep(125000);
+                    Console.WriteLine($"Build ID: {branchNameLastBuild} for Branch: {branch} status: {buildStatus} \nBuild log link: {GetBuildLogsLink(branchNameLastBuild)}\n");
+                }
+                
+            }
+        }
+
+
+        private static void PostCreateBuild(string branchName)
+        {
+            // Post method
+            string postBuildUrl = String.Format($"https://api.appcenter.ms/v0.1/apps/lukaradulovic1/Xamarin-antistress-app/branches/{branchName}/builds");
+            WebRequest postBuildWebRequest = (HttpWebRequest)WebRequest.Create(postBuildUrl);
+            string userTokenPost = "2b2daebe91b83bc7844e7f20b9efb37d5d106d49";
+            postBuildWebRequest.ContentType = "application/json";
+
+
+
+            postBuildWebRequest.Headers.Add("X-API-Token", userTokenPost);
+            postBuildWebRequest.Headers.Add("owner_name", "lukaradulovic1");
+            postBuildWebRequest.Headers.Add("branch", branchName);
+            postBuildWebRequest.Headers.Add("app_name", "Xamarin-antistress-app");
+            postBuildWebRequest.Method = "POST";
+
+
+            using (var streamWriter = new StreamWriter(postBuildWebRequest.GetRequestStream()))
+            {
+                string json = "{\"sourceVersion\":\"613af73288c221cf2b69d2d9c567ef8079300aa5\"," +
+                              "\"debug\": true}";
+
+                streamWriter.Write(json);
+            }
+            HttpWebResponse responseObjectPost = null;
+            responseObjectPost = (HttpWebResponse)postBuildWebRequest.GetResponse();
+        }
+
+        private string GetBranchesList()
+        {
+            // Get list of branches method
+            string userUrlPage = String.Format("https://api.appcenter.ms/v0.1/apps/lukaradulovic1/Xamarin-antistress-app/branches");
+            WebRequest requestObject = WebRequest.Create(userUrlPage);
+            HttpWebResponse responseObjectGet = null;
+            string userTokenGet = "2b2daebe91b83bc7844e7f20b9efb37d5d106d49";
+
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(userTokenGet);
+
+            requestObject.Headers.Add("X-API-Token", userTokenGet);
+            requestObject.Headers.Add("owner_name", "lukaradulovic1");
+            requestObject.Headers.Add("app_name", "Xamarin-antistress-app");
+            requestObject.Method = "GET";
+            responseObjectGet = (HttpWebResponse)requestObject.GetResponse();
+            string branchListResult = null;
+            using (Stream stream = responseObjectGet.GetResponseStream())
+            {
+                StreamReader streamReader = new StreamReader(stream);
+                branchListResult = streamReader.ReadToEnd();
+                streamReader.Close();
+            }
+            return branchListResult;
+        }
+        private static string GetBuildLogsLink(string buildID)
+        {
+            return string.Format($"https://api.appcenter.ms/v0.1/apps/lukaradulovic1/Xamarin-antistress-app/builds/{buildID}/logs?build_id=17&owner_name=lukaradulovic1&app_name=Xamarin-antistress-app");
+        }
+
+    }
+
+}
